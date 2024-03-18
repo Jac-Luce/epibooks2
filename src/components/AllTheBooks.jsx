9aa967ee4efa3a4e41890500c91727fff218ea40
Jac-Luce <luce.jac92@gmail.com>
2024-03-18 18:11:01 +0100

    Aggiunta Commenti

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -1,20 +1,32 @@
-import React, { useState } from 'react'
+import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import fantasy from '../data/fantasy.json';
 import { Row , Container } from 'react-bootstrap';
 import './AllTheBooks.css';
-//import { useState } from 'react';
+import { useState } from 'react';
 import SingleBook from './SingleBook';
 
 export default function AllTheBooks() {
-  //const [inputBook , setInputBook] = useState('');
+  //Stato dell'input
+  const [inputBook , setInputBook] = useState('');
+  //Stato della ricerca
+  const [searchBook , setSearchBook] = useState([fantasy]);
+
+  //Funzione che filtra l'array di libri fantasy
+  function lookFilterBook (el) {
+    setInputBook(el);
+    let filteredBooks = fantasy.filter((book)=> book.title.toLowerCase().includes(inputBook.toLowerCase()));
+    setSearchBook(filteredBooks);
+  }
+
   return (
     <Container>
       <div className='text-center'>
-        <input type="text" placeholder='Cerca libro..' className='mb-3 py-2 ps-3 rounded-pill border border-1'/>
+        <input type="text" placeholder='Cerca libro..' className='mb-3 py-2 ps-3 rounded-pill border border-1' value={inputBook} onChange={(el) => lookFilterBook(el.target.value)}/>
       </div>
       <Row className='g-2'>
-        {fantasy.map((book) => {
+        {
+          searchBook.map((book) => {
           return (
             <SingleBook 
               key= {book.asin}
--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,16 +1,29 @@
 import React from 'react'
 import './AllTheBooks.css';
 import { Col, Card } from 'react-bootstrap';
+import { useState } from 'react';
+import CommentArea from './CommentArea';
 
 export default function SingleBook(props) {
+
+    const {asin, img, title} = props;
+
+    //Stato selezione copertina del libro
+    const [selected, setSelected] = useState(false);
+
+    function redBorderBook() {
+        setSelected(!selected);
+    }
+
   return (
     <>
-        <Col xs={12} md={4} key={props.asin}>
-              <Card className="d-flex flex-column">
-                <Card.Img variant="top" src={props.img} className='img-style'/>
+        <Col xs={12} md={3} key={asin}>
+              <Card className="d-flex flex-column" border={selected ? 'danger' : 'none'} onClick={redBorderBook}>
+                <Card.Img variant="top" src={img} className='img-style'/>
                 <Card.Body>
-                  <Card.Title>{props.title}</Card.Title>
+                  <Card.Title>{title}</Card.Title>
                 </Card.Body>
+                {selected && <CommentArea />}
               </Card>
         </Col>
     </>
